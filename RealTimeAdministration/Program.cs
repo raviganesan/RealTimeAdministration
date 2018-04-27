887a315e9f42d50d7e56e03af70787ad83d91a61
Ravi Ganesan <ravi.ganesan@outlook.com>
2018-04-27 16:46:46 -0500

    added command

--- RealTimeAdministration/Program.cs
+++ RealTimeAdministration/Program.cs
@@ -27,7 +27,7 @@ namespace RealTimeAdministration
                 }
                 else
                 {
-                    Console.WriteLine("Connected");
+                    Console.WriteLine("Administrator Connected");
                 }
 
             }).Wait();
@@ -66,7 +66,7 @@ namespace RealTimeAdministration
                 {
                     isCommand = true;
                     Console.Clear();
-                    Console.WriteLine("Connected");
+                    Console.WriteLine("Administrator Connected");
                 }
 
 
